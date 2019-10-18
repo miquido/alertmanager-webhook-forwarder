be8f70082a9ef11ac15841b26105cad0a3a7e6aa
k911 <konradobal@gmail.com>
2019-10-18 14:01:19 +0200

    feat(server): add "successStatusCode" query parameter
    It could be used to change success status code of http response (some applications require specific status code, e.g.: 200 instead of 204)

--- pkg/alertmanager_webhook_forwarder/cmd/serve.go
+++ pkg/alertmanager_webhook_forwarder/cmd/serve.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 
 	"github.com/miquido/alertmanager-webhook-forwarder/pkg/conditional_runner"
 	"github.com/miquido/alertmanager-webhook-forwarder/pkg/forwarder"
@@ -45,34 +46,77 @@ func providerWebhookHandlerFactory(provider string) func(w http.ResponseWriter, 
 	}
 }
 
+const (
+	errorTypeHTTPMethodNotAllowed            = "HTTP_METHOD_NOT_ALLOWED"
+	errorTypeHTTPBodyContentTypeNotSupported = "HTTP_BODY_CONTENT_TYPE_NOT_SUPPORTED"
+	errorTypeHTTPBodyInvalid                 = "HTTP_BODY_INVALID"
+	errorTypeRequiredQueryParameterMissing   = "REQUIRED_QUERY_PARAMETER_MISSING"
+	errorTypeQueryParameterInvalid           = "QUERY_PARAMETER_INVALID"
+)
+
+func httpJSONError(w http.ResponseWriter, errorType string, errorMessage string, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	if statusCode < 400 || statusCode > 599 {
+		statusCode = 500
+	}
+	w.WriteHeader(statusCode)
+	errorTypeJSON, _ := json.Marshal(errorType)
+	errorMessageJSON, _ := json.Marshal(errorMessage)
+	_, _ = fmt.Fprintln(w, fmt.Sprintf("{\"error\":{\"type\":%s\"message\":%s}}", errorTypeJSON, errorMessageJSON))
+}
+
+func getSuccessStatusCode(r *http.Request) (statusCode int, err error) {
+	successStatusCodeQuery := r.URL.Query().Get("successStatusCode")
+	if successStatusCodeQuery != "" {
+		statusCode, err = strconv.Atoi(successStatusCodeQuery)
+		klog.Infoln("atoi statusCode: ", statusCode, successStatusCodeQuery)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	if statusCode < 200 || statusCode > 299 {
+		statusCode = http.StatusAccepted
+	}
+
+	return statusCode, nil
+}
+
 func providerWebhookHandler(provider string, w http.ResponseWriter, r *http.Request) {
 	conditional_runner.V(7).DumpRequest(r)
 	if r.Method != "POST" {
-		http.Error(w, "Only \"POST\" HTTP Method is allowed", http.StatusMethodNotAllowed)
+		httpJSONError(w, errorTypeHTTPMethodNotAllowed, "Only \"POST\" HTTP Method is allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
 	channelName := r.URL.Query().Get("channel")
 	if channelName == "" {
-		http.Error(w, "Query parameter \"channel\" is required", http.StatusBadRequest)
+		httpJSONError(w, errorTypeRequiredQueryParameterMissing, "Query parameter \"channel\" is required", http.StatusBadRequest)
 		return
 	}
 
 	templateName := r.URL.Query().Get("template")
 	if templateName == "" {
-		http.Error(w, "Query parameter \"template\" is required", http.StatusBadRequest)
+		httpJSONError(w, errorTypeRequiredQueryParameterMissing, "Query parameter \"template\" is required", http.StatusBadRequest)
+		return
+	}
+
+	successStatusCode, err := getSuccessStatusCode(r)
+	if err != nil {
+		httpJSONError(w, errorTypeQueryParameterInvalid, "Value of query parameter \"successStatusCode\" is invalid. Accepted values are numbers between 200 and 299.", http.StatusBadRequest)
 		return
 	}
 
 	if r.Header.Get("Content-Type") != "application/json" {
-		http.Error(w, "Currently accepted body \"content-type\" is only \"application/json\".", http.StatusBadRequest)
+		httpJSONError(w, errorTypeHTTPBodyContentTypeNotSupported, "Currently accepted body \"content-type\" is only \"application/json\".", http.StatusBadRequest)
 		return
 	}
 
 	requestBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		klog.Error(err)
-		http.Error(w, fmt.Sprintf("Error while reading body: %s", err), http.StatusInternalServerError)
+		httpJSONError(w, errorTypeHTTPBodyInvalid, fmt.Sprintf("Error while reading body: %s", err), http.StatusInternalServerError)
 		return
 	}
 
@@ -80,7 +124,7 @@ func providerWebhookHandler(provider string, w http.ResponseWriter, r *http.Requ
 	err = json.Unmarshal(requestBody, &requestBodyMap)
 	if err != nil {
 		klog.Error(err)
-		http.Error(w, fmt.Sprintf("Error while decoding json: %s", err), http.StatusInternalServerError)
+		httpJSONError(w, errorTypeHTTPBodyInvalid, fmt.Sprintf("Error while decoding json: %s", err), http.StatusBadRequest)
 		return
 	}
 
@@ -88,8 +132,10 @@ func providerWebhookHandler(provider string, w http.ResponseWriter, r *http.Requ
 		_ = forwarder.Forward(provider, channelName, templateName, requestBodyMap)
 	}()
 
-	w.WriteHeader(http.StatusAccepted)
-	_, _ = fmt.Fprint(w, "Accepted")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(int(successStatusCode))
+	_, _ = fmt.Fprint(w, "{\"status\":\"accepted\"}")
 }
 
 func init() {
